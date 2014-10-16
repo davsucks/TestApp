e912d8c5f36508102bde1c1f4d6ad0c87a7cf1b0
David <davsucks@umich.edu>
2014-10-15 20:40:15 -0400

    Basic functionality of displaying user input finished

--- app/src/main/java/com/example/david/testapp/MyActivity.java
+++ app/src/main/java/com/example/david/testapp/MyActivity.java
@@ -1,12 +1,16 @@
 package com.example.david.testapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends Activity {
+    public final static String EXTRA_MESSAGE = "com.example.TestApp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +36,15 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button **/
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
